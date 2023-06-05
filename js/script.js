61e9bb899b979d96873a126984cbaf7bcbf28a92
Kul_Nik <nik_5678@mail.ru>
2023-06-05 20:16:16 +0300

    Ti-9 new js code and dark-theme

---
+++ js/script.js
@@ -0,0 +1,99 @@
+'use strict';
+/*
+const data = [ 
+    {
+        skillName: "html",
+        skillLevel: 50,
+        iconName: "html.svg"
+    }, 
+    {
+        skillName: "css",
+        skillLevel: 40,
+        iconName: "css.svg"
+    }, 
+    {
+        skillName: "c++",
+        skillLevel: 70,
+        iconName: "c++.svg"
+    }, 
+    {
+        skillName: "php",
+        skillLevel: 20,
+        iconName: "php.svg"
+    }
+];
+
+let skillList = document.querySelector('dl.skill-list');
+
+data.forEach(function(item) {
+    
+    const dt = document.createElement('dt');
+    const dd = document.createElement('dd');
+    const div = document.createElement('div');
+    
+    dt.classList.add('skill-item');
+    dd.classList.add('skill-level');
+
+    dt.textContent = item.skillName;
+    div.textContent = item.skillLevel + '%';
+
+    div.style.width = item.skillLevel + '%';
+    dt.style.backgroundImage = `url('../img/${item.iconName}')`;
+    
+    dd.append(div);
+
+    skillList.append(dt);
+    skillList.append(dd);
+});
+skills.data.forEach(function(item) {
+*/
+
+const skills = {
+    data: [ 
+        {
+            skillName: "html",
+            skillLevel: 50,
+            iconName: "html.svg"
+        }, 
+        {
+            skillName: "css",
+            skillLevel: 40,
+            iconName: "css.svg"
+        }, 
+        {
+            skillName: "c++",
+            skillLevel: 70,
+            iconName: "c++.svg"
+        }, 
+        {
+            skillName: "php",
+            skillLevel: 20,
+            iconName: "php.svg"
+        }
+    ],
+    generateList: function(skillList) {
+        skills.data.forEach(function(item) {
+            const dt = document.createElement('dt');
+            const dd = document.createElement('dd');
+            const div = document.createElement('div');
+            
+            dt.classList.add('skill-item');
+            dd.classList.add('skill-level');
+        
+            dt.textContent = item.skillName;
+            div.textContent = item.skillLevel + '%';
+        
+            div.style.width = item.skillLevel + '%';
+            dt.style.backgroundImage = `url('../img/${item.iconName}')`;
+            
+            dd.append(div);
+        
+            skillList.append(dt);
+            skillList.append(dd);
+        });
+    }
+};
+
+let skillList = document.querySelector('dl.skill-list');
+
+skills.generateList(skillList);
