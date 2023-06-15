92d836ea89ba4c5b32595f3b862d7c14aadaf2ce
Kul_Nik <nik_5678@mail.ru>
2023-06-15 19:35:24 +0300

    solve problems of ti-9

--- js/script.js
+++ js/script.js
@@ -1,52 +1,4 @@
 'use strict';
-/*
-const data = [ 
-    {
-        skillName: "html",
-        skillLevel: 50,
-        iconName: "html.svg"
-    }, 
-    {
-        skillName: "css",
-        skillLevel: 40,
-        iconName: "css.svg"
-    }, 
-    {
-        skillName: "c++",
-        skillLevel: 70,
-        iconName: "c++.svg"
-    }, 
-    {
-        skillName: "php",
-        skillLevel: 20,
-        iconName: "php.svg"
-    }
-];
-
-let skillList = document.querySelector('dl.skill-list');
-
-data.forEach(function(item) {
-    
-    const dt = document.createElement('dt');
-    const dd = document.createElement('dd');
-    const div = document.createElement('div');
-    
-    dt.classList.add('skill-item');
-    dd.classList.add('skill-level');
-
-    dt.textContent = item.skillName;
-    div.textContent = item.skillLevel + '%';
-
-    div.style.width = item.skillLevel + '%';
-    dt.style.backgroundImage = `url('../img/${item.iconName}')`;
-    
-    dd.append(div);
-
-    skillList.append(dt);
-    skillList.append(dd);
-});
-skills.data.forEach(function(item) {
-*/
 
 const skills = {
     data: [ 
@@ -72,7 +24,7 @@ const skills = {
         }
     ],
     generateList: function(skillList) {
-        skills.data.forEach(function(item) {
+        this.data.forEach(function(item) {
             const dt = document.createElement('dt');
             const dd = document.createElement('dd');
             const div = document.createElement('div');
@@ -81,19 +33,18 @@ const skills = {
             dd.classList.add('skill-level');
         
             dt.textContent = item.skillName;
-            div.textContent = item.skillLevel + '%';
+            div.textContent = `${item.skillLevel}%`;
         
-            div.style.width = item.skillLevel + '%';
-            dt.style.backgroundImage = `url("../img/${item.iconName}")`;
+            div.style.width = `${item.skillLevel}%`;
+            dt.style.backgroundImage = `url("img/${item.iconName}")`;
            
             dd.append(div);
         
-            skillList.append(dt);
-            skillList.append(dd);
+            skillList.append(dt, dd);
         });
     }
 };
 
-let skillList = document.querySelector('dl.skill-list');
+const skillList = document.querySelector('dl.skill-list');
 
 skills.generateList(skillList);
